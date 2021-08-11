aa08ebd0d01b25b90890ed388c8c39a4b5efe5c0
Samoel Lopes Costa <samuellopescosta@gmail.com>
2021-08-11 16:38:30 -0300

    iniciando projeto

---
+++ etapas.js
@@ -0,0 +1,55 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+
+            {
+                numero: '90900',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '90900.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '91900',
+                nome: 'Beltrano de Silva',
+                partido: 'DFG',
+                fotos: [
+                    { url: '91900.jpg', legenda: 'Vereador' }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numeros: '91',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Vice-ciclano',
+                fotos: [
+                    { url: '91.png', legenda: 'Prefeito' },
+                    { url: '91_1.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+
+            },
+            {
+                numeros: '90',
+                nome: 'Rick',
+                partido: 'ABC',
+                vice: 'morty',
+                fotos: [
+                    { url: '90.jpg', legenda: 'Prefeito' },
+                    { url: '90_1.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+
+            }
+        ]
+
+    }
+
+]
